982da8ff1c4d10eebddeedb00a766f092b4ff19b
Kit Sczudlo <kit@scuzzstuff.com>
2015-12-24 11:27:41 -0800

    Make a few pieces more idiomatically "rusty" (while-let), add some TODOs

--- src/main.rs
+++ src/main.rs
@@ -13,6 +13,7 @@ use std::os::unix::io::RawFd;
 
 use termios::*;
 
+// TODO: pass in baud rate
 fn setup_fd(fd: RawFd) -> io::Result<()> {
     let mut termios = try!(Termios::from_fd(fd));
 
@@ -21,6 +22,7 @@ fn setup_fd(fd: RawFd) -> io::Result<()> {
     termios.c_cflag = CS8 | CREAD | CLOCAL;
     termios.c_lflag = 0;
 
+    // TODO: figure out why 115200 isn't an option!
     try!(cfsetspeed(&mut termios, B9600));
     try!(tcsetattr(fd, TCSANOW, &termios));
     try!(tcflush(fd, TCIOFLUSH));
@@ -29,20 +31,25 @@ fn setup_fd(fd: RawFd) -> io::Result<()> {
 }
 
 fn main() {
-    let mut ser_file = File::open("/dev/ttyUSB0").ok().expect("Unable to open file handle!");
+    // TODO: option parsing for serial port, baud, and hardware/software handshaking
+    let mut ser_file = File::open("/dev/ttyUSB0").ok().expect("Unable to open file handle to serial port!");
 
     setup_fd(ser_file.as_raw_fd()).ok().expect("Unable to configure serial port!");
 
+    // TODO: Is there a way to limit the existence of "buffer" to the loop?  I don't really need
+    //       an eternal mutable buffer reference
     let mut buffer = [0; 256];
-    let mut buf_len = 0;
-    
-    while match ser_file.read(&mut buffer) { Ok(n) => {buf_len = n; true}, Err(_) => false} {
-        if buf_len <= 0 {
+    while let Ok(n) = ser_file.read(&mut buffer) {
+        // Catch EOF and other "not-quite-errors"
+        if n <= 0 {
             break;
         }
-        print!("{}", str::from_utf8(&buffer[..buf_len]).ok().expect("Oh noes unable to parse as string!"));
-        assert!(stdout().flush().is_ok());
+
+        // Woo! Data! Print and flush
+        print!("{}", str::from_utf8(&buffer[..n]).unwrap());
+        stdout().flush().ok().expect("Unable to flush stdout!");
     }
 
+    // Make sure we print out a newline to cap off output
     println!("");
 }
